4371d4bb9279985e2bb526af937fb53666094b71
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-12-13 08:19:47 -0300

    Update model.py

--- restaurante/models/model.py
+++ restaurante/models/model.py
@@ -43,3 +43,8 @@ class Funcionario(db.Model):
     email=db.Column(db.String(120), unique=True, nullable=False)
     cpf=db.Column(db.String(120), unique=True, nullable=False)
 
+class Cardapio(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(80), nullable=False)
+    preco=db.Column(db.String(80), nullable=False)
+
